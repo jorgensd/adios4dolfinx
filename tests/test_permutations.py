f10ffdf6f5f11e686464c7d992efab2c4db1839c
Jorgen S. Dokken <dokken92@gmail.com>
2023-02-24 13:13:03 +0000

    All tests passing

--- tests/test_permutations.py
+++ tests/test_permutations.py
@@ -7,7 +7,6 @@ import ufl
 from mpi4py import MPI
 
 import adios4dolfinx
-from adios4dolfinx import read_function_perm, read_mesh, write_mesh_perm
 
 
 def write_function(mesh, el, f) -> str:
@@ -97,7 +96,7 @@ def test_read_write_2D(read_comm, write_comm, family, degree, cell_type):
 @pytest.mark.parametrize("read_comm", [MPI.COMM_SELF, MPI.COMM_WORLD])
 @pytest.mark.parametrize("write_comm", [MPI.COMM_SELF, MPI.COMM_WORLD])
 def test_read_write_3D(read_comm, write_comm, family, degree):
-    mesh = dolfinx.mesh.create_unit_cube(write_comm, 3, 3, 3)
+    mesh = dolfinx.mesh.create_unit_cube(write_comm, 3, 5, 7)
     el = ufl.FiniteElement(family, mesh.ufl_cell(), degree)
 
     def f(x):
@@ -139,30 +138,3 @@ def test_read_write_hex(read_comm, write_comm, family, degree):
     hash = write_function(mesh, el, f)
     MPI.COMM_WORLD.Barrier()
     read_function(read_comm, el, f, hash)
-
-
-# @pytest.mark.parametrize("encoder, suffix", [("BP4", ".bp"), ("HDF5", ".h5"), ("BP5", ".bp")])
-# @pytest.mark.parametrize("ghost_mode", [dolfinx.mesh.GhostMode.shared_facet])
-# def test_mesh_read_writer(encoder, suffix, ghost_mode):
-#     encoder = "BP4"
-#     suffix = ".bp"
-#     N = 25
-#     ghost_mode = dolfinx.mesh.GhostMode.shared_facet
-#     file = pathlib.Path(f"output/adios_mesh_{encoder}")
-#     if MPI.COMM_WORLD.rank == 0:
-#         mesh_loc = dolfinx.mesh.create_unit_square(MPI.COMM_SELF, N, N, ghost_mode=ghost_mode)
-#         write_mesh_perm(mesh_loc, file.with_suffix(suffix), encoder)
-#         V = dolfinx.fem.FunctionSpace(mesh_loc, ("N1curl", 1))
-#         u = dolfinx.fem.Function(V)
-#         u.interpolate(lambda x: (x[0], 100*x[1]+3))
-#         write_function(u, file.with_suffix(suffix), encoder)
-#     MPI.COMM_WORLD.Barrier()
-
-#     mesh = read_mesh(MPI.COMM_WORLD, file.with_suffix(suffix), encoder, dolfinx.mesh.GhostMode.shared_facet)
-#     V = dolfinx.fem.FunctionSpace(mesh, ("N1curl", 1))
-#     u = dolfinx.fem.Function(V)
-
-#     read_function_perm(u, file.with_suffix(suffix), encoder)
-#     w = dolfinx.fem.Function(V)
-#     w.interpolate(lambda x: (x[0], 100*x[1]+3))
-#     assert np.allclose(w.x.array, u.x.array)
