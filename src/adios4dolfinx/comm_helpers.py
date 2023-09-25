c60c8b939f6bc42eb4b0f453a342f6ba763f85b7
Joergen Schartum Dokken <dokken@simula.no>
2023-09-25 11:34:29 +0000

    Flake8

--- src/adios4dolfinx/comm_helpers.py
+++ src/adios4dolfinx/comm_helpers.py
@@ -261,7 +261,7 @@ def send_dofs_and_recv_values(
     r_msg = [inc_dofs, recv_size, MPI.INT64_T]
     dofmap_to_values.Neighbor_alltoallv(s_msg, r_msg)
     dofmap_to_values.Free()
-   
+
     # Send back appropriate input values
     sending_values = np.zeros(len(inc_dofs), dtype=input_array.dtype)
     for i, dof in enumerate(inc_dofs):
@@ -276,7 +276,7 @@ def send_dofs_and_recv_values(
 
     # Sort inputs according to local dof number (input process)
     values = np.empty_like(inc_values, dtype=input_array.dtype)
-    
+
     for i in range(len(dest_ranks)):
         for j in range(out_size[i]):
             in_pos = dofs_offsets[i] + j
