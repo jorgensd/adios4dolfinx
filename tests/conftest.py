Fix paths for MPI by broadcasting from rank 0

tests/conftest.py
@@ -33,9 +33,10 @@ def write_function(tmp_path):
             .replace("[", "")
             .replace("]", "")
         )
-
+        # Consistent tmp dir across processes
+        f_path = MPI.COMM_WORLD.bcast(tmp_path, root=0)
         file_hash = f"{el_hash}_{np.dtype(dtype).name}"
-        filename = tmp_path / f"mesh_{file_hash}.bp"
+        filename = f_path / f"mesh_{file_hash}.bp"
         if mesh.comm.size != 1:
             if not append:
                 adios4dolfinx.write_mesh(filename, mesh)
@@ -46,21 +47,20 @@ def write_function(tmp_path):
                     adios4dolfinx.write_mesh(filename, mesh)
                 adios4dolfinx.write_function(filename, uh, time=0.0)
 
-        return file_hash
+        return filename
 
     return _write_function
 
 
 @pytest.fixture(scope="function")
-def read_function(tmp_path):
-    def _read_function(comm, el, f, hash, dtype, name="uh"):
-        filename = tmp_path / f"mesh_{hash}.bp"
+def read_function():
+    def _read_function(comm, el, f, path, dtype, name="uh"):
         engine = "BP4"
-        mesh = adios4dolfinx.read_mesh(filename, comm, engine, dolfinx.mesh.GhostMode.shared_facet)
+        mesh = adios4dolfinx.read_mesh(path, comm, engine, dolfinx.mesh.GhostMode.shared_facet)
         V = dolfinx.fem.functionspace(mesh, el)
         v = dolfinx.fem.Function(V, dtype=dtype)
         v.name = name
-        adios4dolfinx.read_function(filename, v, engine)
+        adios4dolfinx.read_function(path, v, engine)
         v_ex = dolfinx.fem.Function(V, dtype=dtype)
         v_ex.interpolate(f)
 
@@ -107,7 +107,9 @@ def write_function_time_dep(tmp_path):
             .replace("]", "")
         )
         file_hash = f"{el_hash}_{np.dtype(dtype).name}"
-        filename = tmp_path / f"mesh_{file_hash}.bp"
+        # Consistent tmp dir across processes
+        f_path = MPI.COMM_WORLD.bcast(tmp_path, root=0)
+        filename = f_path / f"mesh_{file_hash}.bp"
         if mesh.comm.size != 1:
             adios4dolfinx.write_mesh(filename, mesh)
             adios4dolfinx.write_function(filename, uh, time=t0)
@@ -121,28 +123,27 @@ def write_function_time_dep(tmp_path):
                 uh.interpolate(f1)
                 adios4dolfinx.write_function(filename, uh, time=t1)
 
-        return file_hash
+        return filename
 
     return _write_function_time_dep
 
 
 @pytest.fixture(scope="function")
-def read_function_time_dep(tmp_path):
-    def _read_function_time_dep(comm, el, f0, f1, t0, t1, hash, dtype):
-        filename = tmp_path / f"mesh_{hash}.bp"
+def read_function_time_dep():
+    def _read_function_time_dep(comm, el, f0, f1, t0, t1, path, dtype):
         engine = "BP4"
-        mesh = adios4dolfinx.read_mesh(filename, comm, engine, dolfinx.mesh.GhostMode.shared_facet)
+        mesh = adios4dolfinx.read_mesh(path, comm, engine, dolfinx.mesh.GhostMode.shared_facet)
         V = dolfinx.fem.functionspace(mesh, el)
         v = dolfinx.fem.Function(V, dtype=dtype)
 
-        adios4dolfinx.read_function(filename, v, engine, time=t1)
+        adios4dolfinx.read_function(path, v, engine, time=t1)
         v_ex = dolfinx.fem.Function(V, dtype=dtype)
         v_ex.interpolate(f1)
 
         res = np.finfo(dtype).resolution
         assert np.allclose(v.x.array, v_ex.x.array, atol=10 * res, rtol=10 * res)
 
-        adios4dolfinx.read_function(filename, v, engine, time=t0)
+        adios4dolfinx.read_function(path, v, engine, time=t0)
         v_ex = dolfinx.fem.Function(V, dtype=dtype)
         v_ex.interpolate(f0)
 

tests/test_checkpointing_vector.py
@@ -67,9 +67,9 @@ def test_read_write_2D(
             values[1] += 2j * x[0]
         return values
 
-    hash = write_function(mesh, el, f, f_dtype)
+    fname = write_function(mesh, el, f, f_dtype)
     MPI.COMM_WORLD.Barrier()
-    read_function(read_comm, el, f, hash, f_dtype)
+    read_function(read_comm, el, f, fname, f_dtype)
 
 
 @pytest.mark.parametrize("is_complex", [True, False])
@@ -93,9 +93,9 @@ def test_read_write_3D(
             values[1] += 2j * np.cos(x[2])
         return values
 
-    hash = write_function(mesh, el, f, dtype=f_dtype)
+    fname = write_function(mesh, el, f, dtype=f_dtype)
     MPI.COMM_WORLD.Barrier()
-    read_function(read_comm, el, f, hash, dtype=f_dtype)
+    read_function(read_comm, el, f, fname, dtype=f_dtype)
 
 
 @pytest.mark.parametrize("is_complex", [True, False])